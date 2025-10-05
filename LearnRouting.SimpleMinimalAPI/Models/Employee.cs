e1a2a7f5c5553a162bf7471c58cde97da4efd28a
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-05 20:29:43 +0500

    Добавление модели для Employee и репо с CRUD операциями

---
+++ LearnRouting.SimpleMinimalAPI/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace LearnRouting.SimpleMinimalAPI.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Position { get; set; }
+    public double Salary { get; set; }
+
+    public Employee(int id, string name, string position, double salary)
+    {
+        Id = id;
+        Name = name;
+        Position = position;
+        Salary = salary;
+    }
+}
