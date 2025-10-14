c3fc9752124e40b92ab500d24bc98de1f94e3d06
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-14 12:28:47 +0500

    Изучен Route

---
+++ LearnMVCControllers/Controllers/DepartmensController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearnMVCControllers.Controllers;
+
+[Route("/api")]
+public class DepartmensController
+{
+    [HttpGet("departmens")]
+    public string GetDepartments()
+    {
+        return "These are the departments";
+    }
+
+    [HttpGet("departmens/{id}")]
+    public string GetDepartmentById(int id)
+    {
+        return $"Department info: {id}";
+    }
+
+    [NonAction]
+    public string GetDepartmentByName(string name)
+    {
+        return "abc";
+    }
+}
