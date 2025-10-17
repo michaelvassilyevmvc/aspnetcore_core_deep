4cfa04d12756b3c79b88e9f2933b6249ca66aab5
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-17 14:36:41 +0500

    Реализован crud контроллер и интерфейсы на чистом html

---
+++ DepartsCrud/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DepartsCrud.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return Content("Welcome to the departments management.");
+    }
+}
