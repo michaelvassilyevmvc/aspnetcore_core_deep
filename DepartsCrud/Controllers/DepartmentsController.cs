909bc9daf6f8fda857dd75b6ec9ed9bdeb9ff9c6
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-25 15:08:55 +0500

    ch11_L12_self_end Добавлены стили для страниц Create, Details, Index

--- DepartsCrud/Controllers/DepartmentsController.cs
+++ DepartsCrud/Controllers/DepartmentsController.cs
@@ -44,15 +44,7 @@ public class DepartmentsController : Controller
     [HttpGet]
     public IActionResult Create()
     {
-        var html = @"
-            <h1>Add Department</h1>
-            <form method='post' action='/departments/create'>
-            <label>Name: <input type='text' name='Name' /></label>
-            <label>Description: <input type='text' name='Description' /></label>
-            <br />
-            <button type='submit'>Add</button>
-            </form>";
-        return Content(html, "text/html");
+        return View();
     }
 
     [HttpPost]
