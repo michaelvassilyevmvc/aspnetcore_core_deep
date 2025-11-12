3f9bece880439edaf860a7ae830062c2d2eb2dc2
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-12 11:00:45 +0500

    ch13_L9_end Добавлен ViewComponent для инкапсуляции partitialView и данных работающих с ней

---
+++ DepartsCrud/Views/Shared/Components/DepartmentList/DepartmentListViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using DepartsCrud.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DepartsCrud.Views.Shared.Components.DepartmentList;
+
+[ViewComponent]
+public class DepartmentListViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        var departments = DepartmentsRepository.GetDepartments();
+        return View(departments);
+    }
+}
