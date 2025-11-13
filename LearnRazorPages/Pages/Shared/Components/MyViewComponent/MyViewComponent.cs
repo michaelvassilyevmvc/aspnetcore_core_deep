ce0de2efd8df96183b38001651a56d433192c61d
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-13 22:59:59 +0500

    ch14_L13_end Использование PartialView и Component в Razor

---
+++ LearnRazorPages/Pages/Shared/Components/MyViewComponent/MyViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearnRazorPages.Pages.Shared.Components.MyViewComponent;
+
+[ViewComponent(Name ="MyViewComponent")]
+public class MyViewComponent: ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
