c099090371246ebad5ca0cd069d5c6fa112ae560
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-13 13:53:03 +0500

    ch14_L9_end Привязка свойств через атрибуты

--- LearnRazorPages/Pages/Employees/Edit.cshtml.cs
+++ LearnRazorPages/Pages/Employees/Edit.cshtml.cs
@@ -1,14 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Cryptography;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace LearnRazorPages.Pages.Employees;
 
+
 public class Edit : PageModel
 {
-    [BindProperty(SupportsGet = true)]
-    public string Id { get; set; }
-    public void OnGet(string id)
+    [BindProperty(SupportsGet = true)] public string Id { get; set; }
+
+    [BindProperty]
+    public InputModel? InputModel { get; set; }
+    public void OnGet()
     {
-        
     }
+
+    public void OnPostSave()
+    {
+    }
+
+    public void OnPostDelete()
+    {
+    }
+}
+
+public class InputModel
+{
+    public int? Id { get; set; }
+    public string? Name { get; set; }
 }
