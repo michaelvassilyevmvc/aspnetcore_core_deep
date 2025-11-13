d26548105268d22fbb8edc703c2712cc8c3fe77c
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-13 22:13:02 +0500

    ch14_L10_end Использование ModelState для проверки валидации

--- LearnRazorPages/Pages/Employees/Edit.cshtml.cs
+++ LearnRazorPages/Pages/Employees/Edit.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -17,15 +18,33 @@ public class Edit : PageModel
 
     public void OnPostSave()
     {
+        if (!ModelState.IsValid)
+        {
+            InputModel.ErrorMessage = GetErrors();
+        }
     }
 
     public void OnPostDelete()
     {
     }
+    
+    private List<string> GetErrors()
+    {
+        var errorMessages = new List<string>();
+        foreach (var value in ModelState.Values)
+        foreach (var error in value.Errors)
+            errorMessages.Add(error.ErrorMessage);
+
+        return errorMessages;
+    }
 }
 
 public class InputModel
 {
+    
     public int? Id { get; set; }
+    [Required]
     public string? Name { get; set; }
+
+    public List<string>? ErrorMessage { get; set; }
 }
