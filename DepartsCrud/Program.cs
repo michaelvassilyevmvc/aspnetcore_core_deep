46c34fd00393d8435ab678f914cb0077c6771f9d
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-17 23:00:34 +0500

    ch15_L4_end Добавлена поддержка Razor Pages

--- DepartsCrud/Program.cs
+++ DepartsCrud/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -11,6 +12,7 @@ app.UseEndpoints(endpoints =>
             "default",
             "{controller=Home}/{action=Index}/{id?}"
         );
+        endpoints.MapRazorPages();
     }
 );
 
