bf6e14bf4b926bf0732c500579b75a4a44aa8494
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-18 09:25:10 +0500

    ch15_L5_end Добавлена страница отображения Employees List

---
+++ DepartsCrud/Pages/Employees/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using DepartsCrud.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DepartsCrud.Pages.Employees;
+
+public class Index : PageModel
+{
+    public List<Employee>? Employees { get; set; }
+    public void OnGet()
+    {
+        this.Employees = EmployeesRepository.GetEmployees();
+        
+    }
+}
