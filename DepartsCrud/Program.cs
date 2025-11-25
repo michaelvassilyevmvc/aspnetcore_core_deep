9330ec0c778565c687703de7686d0b2f106e9b4c
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-25 11:09:04 +0500

    ch16_L7_end Добавление статических ресурсов и asp-append-version для контроля их версионности

--- DepartsCrud/Program.cs
+++ DepartsCrud/Program.cs
@@ -5,6 +5,15 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
+app.UseStaticFiles(new StaticFileOptions()
+{
+    OnPrepareResponse = ctx =>
+    {
+        ctx.Context.Response.Headers["Cache-Control"] = "public, max-age=600";
+        ctx.Context.Response.Headers["Expires"] = DateTime.UtcNow.AddMinutes(10).ToString("R");
+    }
+            
+});
 app.UseRouting();
 app.UseEndpoints(endpoints =>
     {
